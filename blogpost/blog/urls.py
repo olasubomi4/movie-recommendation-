66fa95b8731c86b1e6d72c8c77a20a529ecadee6
Odekunle Olasubomi <40355148+olasubomi4@users.noreply.github.com>
2022-06-28 12:06:54 +0100

    Update urls.py

--- blogpost/blog/urls.py
+++ blogpost/blog/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 	url(r'^search/$', views.search, name='search'),
 	url(r'^main/$', views.main, name='main'),
 
-	#path('search/movie/', test.as_view(), name='original_title'),
+	path('search/movie/', test.as_view(), name='original_title'),
 
    # path('home/', HomePageView.as_view(), name='home'),
    # #path('post/', CreatePostView.as_view(), name='add_post') ,#
